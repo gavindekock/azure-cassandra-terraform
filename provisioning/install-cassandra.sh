b72a3787f33ecf28250ac6bc8320d486dc07479b
Tim Park <timfpark@gmail.com>
2018-01-02 09:05:09 -0800

    fix LOCATION and move chown /mtn/cassandra to after Cassandra install

--- provisioning/install-cassandra.sh
+++ provisioning/install-cassandra.sh
@@ -1,10 +1,10 @@
 sudo mkdir /mnt/cassandra
 sudo mkdir /mnt/cassandra/data
-sudo chown -R cassandra:cassandra /mnt/cassandra
 
 sudo apt-get update
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 0x219BD9C9
 sudo apt-get install -y python-pip
+sudo apt-get install -y jq
 sudo pip install --upgrade pip
 sudo chown -R ops:ops /usr/local/lib/python2.7/dist-packages/
 pip install cassandra-driver
@@ -14,8 +14,8 @@ curl https://www.apache.org/dist/cassandra/KEYS | sudo apt-key add -
 sudo apt-key adv --keyserver pool.sks-keyservers.net --recv-key A278B781FE4B2BDA
 sudo apt-get update
 sudo apt-get install -y cassandra
-sudo apt-get install -y cassandra-tools
 sudo service cassandra stop
+sudo apt-get install -y cassandra-tools
 sudo rm -rf /var/lib/cassandra/data/system/*
 sudo sed -i "s/cluster_name: 'Test Cluster'/cluster_name: 'cassandra_cluster'/g" /etc/cassandra/cassandra.yaml
 
@@ -32,12 +32,14 @@ sudo sed -i "s/endpoint_snitch: SimpleSnitch/endpoint_snitch: GossipingPropertyF
 sudo sed -i "s/# broadcast_rpc_address: 1.2.3.4/broadcast_rpc_address: $PRIVATEIP/g" /etc/cassandra/cassandra.yaml
 sudo sed -i "s/authenticator: AllowAllAuthenticator/authenticator: PasswordAuthenticator/g" /etc/cassandra/cassandra.yaml
 
-export FD=$( curl -H Metadata:true "http://169.254.169.254/metadata/instance/compute/platformFaultDomain?api-version=2017-04-02&format=text" )
-export LOCATION=$(curl -H Metadata:true "http://169.254.169.254/metadata/instance/compute/location?api-version=2017-07-01&format=text")
+export FD=$(curl -H Metadata:true "http://169.254.169.254/metadata/instance/compute/platformFaultDomain?api-version=2017-04-02&format=text")
+#export LOCATION=$(curl -H Metadata:true "http://169.254.169.254/metadata/instance/compute/location?api-version=2017-07-01&format=text")
+export LOCATION=$(curl -H Metadata:true "http://169.254.169.254/metadata/instance?api-version=2017-04-02" | jq -r '.compute.location')
 
 sudo rm -f /etc/cassandra/cassandra-rackdc.properties
 sudo touch /etc/cassandra/cassandra-rackdc.properties
 sudo chown -R ops:ops /etc/cassandra
+sudo chown -R cassandra:cassandra /mnt/cassandra
 
 sudo echo dc=dc-$LOCATION > /etc/cassandra/cassandra-rackdc.properties
 sudo echo rack=rack-$FD >> /etc/cassandra/cassandra-rackdc.properties
